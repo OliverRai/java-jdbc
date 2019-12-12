30b747fa567f32696843c260d7eb54cb3043c11d
OliverRai <raiane.mateus.oliveira@gmail.com>
2019-12-12 17:12:27 -0300

    simple date format

--- src/br/com/caelum/jdbc/teste/TestaLista.java
+++ src/br/com/caelum/jdbc/teste/TestaLista.java
@@ -1,6 +1,8 @@
 package br.com.caelum.jdbc.teste;
 
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import br.com.caelum.jdbc.dao.ContatoDao;
@@ -16,8 +18,13 @@ public class TestaLista {
 		System.out.println("Nome: " + contato.getNome());
 		System.out.println("Email: " + contato.getEmail());
 		System.out.println("Endereço: " + contato.getEndereco());
-		System.out.println("Data de Nascimento: " +
-		contato.getDataNascimento().getTime() + "\n");
+	//	System.out.println("Data de Nascimento: " +
+		//contato.getDataNascimento().getTime() + "\n");
+		//Padrao SQL
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+		//yyyy = Ano,MM = Mês,dd=Dia,hh  = Hora(HH maiusculo padrao 24hrs) e mm é minuto
+		String dataAtual = sdf.format(new Date());
+		System.out.println("Data: " + dataAtual);
 		}
 	}
 }
