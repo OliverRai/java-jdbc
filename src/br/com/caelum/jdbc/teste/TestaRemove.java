b8c8a58e073453cbb422a73e13cfccb7df01b3d4
OliverRai <raiane.mateus.oliveira@gmail.com>
2019-12-12 17:35:40 -0300

    metodo remove

--- src/br/com/caelum/jdbc/teste/TestaRemove.java
+++ src/br/com/caelum/jdbc/teste/TestaRemove.java
@@ -1,6 +1,7 @@
 package br.com.caelum.jdbc.teste;
 
 import java.sql.SQLException;
+import java.util.Calendar;
 
 import br.com.caelum.jdbc.dao.ContatoDao;
 import br.com.caelum.jdbc.modelo.Contato;
@@ -9,6 +10,9 @@ public class TestaRemove {
 	public static void main(String[] args) throws SQLException {
 		Contato contato = new Contato();
 		ContatoDao dao = new ContatoDao();
+		
+		long id = 3; 
+		contato.setId(id);
 		dao.remove(contato);
 	}
 }
